111206bebd50cd2a0f97fa18bb3424255316962a
mireia <mireia@actualweb.es>
2018-05-11 08:33:16 +0200

    adding a properties

--- object.js
+++ object.js
@@ -2,6 +2,10 @@
 //Ways of creating an object
 // 1rst
 var obj = {};
+//add atribute by dot nonation
+obj.param = 'new value';
+//adding by brackets. Useful for adding variables inside
+obj["param2"] = 'new value';
 
 // 2nd
 var nextObject = Object.create(Object.prototype);
---
+++ tasks/task.js
@@ -0,0 +1,5 @@
+var task = {
+    title: 'My title',
+    description: 'My description'
+};
+task.author = 'Me';
