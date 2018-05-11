814d4f449466f22b05a0e4a7226ebfdefa47e816
mireia <mireia@actualweb.es>
2018-05-11 08:55:47 +0200

    Definiendo las propidades del objeto

--- tasks/task.js
+++ tasks/task.js
@@ -3,3 +3,20 @@ var task = {
     description: 'My description'
 };
 task.author = 'Me';
+task.toString = function () {
+    'use strict';
+    return this.title + ' ' + this.description;
+};
+
+Object.defineProperty(task, 'toString', {
+    value: function () {
+        'use strict';
+        return this.title + ' ' + this.description;
+    },
+    writable: false, // i don't want to reasign this
+    enumerable: false, //hide for everyboody else
+    configurable: false // is not configurable later with defineProperty
+});
+
+console.log(Object.keys(task));
+console.log(task.toString());
