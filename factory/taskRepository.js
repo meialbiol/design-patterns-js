09a39d6e46574b4044658dca22c01478b89c976d
mireia <mireia@actualweb.es>
2018-05-14 10:15:52 +0200

    Init files from Factory Pattern

---
+++ factory/taskRepository.js
@@ -0,0 +1,20 @@
+//This is a module
+var repo = function (){
+    var db = {};
+    var get = function (id) {
+            console.log('Getting task '+id);
+            return {
+                name: 'new task from db'
+            }
+        };
+    var save = function (task) {
+        console.log('Saving '+ task.name+ ' to the db');
+    };
+
+    return {
+          get: get,
+          save: save
+      }
+};
+
+module.exports = repo();
