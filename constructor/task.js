96d04dd14f72d338f21dd8ff58bf23b707603b0b
mireia <mireia@actualweb.es>
2018-05-14 08:50:00 +0200

    Adapting js to nodejs

--- constructor/task.js
+++ constructor/task.js
@@ -2,23 +2,16 @@ var Task = function (name) {
     'use strict';
     this.name = name;
     this.completed = false;
-
-    this.completed = function () {
-        console.log('completing Task: ' + this.name);
-        this.completed = true;
-    };
-
-    this.save = function () {
-        console.log('saving Task: ' + this.name);
-    };
 };
 
-var task1 = new Task('create a demo for constructors');
-var task2 = new Task('create a demo for modules');
-var task3 = new Task('create a demo for singletons');
-var task4 = new Task('create a demo for prototypes');
+Task.prototype.complete = function () {
+    'use strict';
+    console.log('completing Task: ' + this.name);
+    this.completed = true;
+};
 
-task1.completed();
-task2.save();
-task3.save();
-task4.save();
+Task.prototype.save = function () {
+    console.log('saving Task: ' + this.name);
+};
+
+module.exports = Task;
