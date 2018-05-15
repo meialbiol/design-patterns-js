9547f8e4d14cd275a687820b22300c036491a30d
mireia <mireia@actualweb.es>
2018-05-15 07:59:46 +0200

    Factory Patern

--- factory/main.js
+++ factory/main.js
@@ -1,5 +1,5 @@
 var Task = require('./task');
-var repoFactory = require('./repoFactorywCache');
+var repoFactory = require('./repoFactory2');
 
 var task1 = new Task(repoFactory.getRepo('task').get(1));
 var task2 = new Task(repoFactory.getRepo('task').get(2));
