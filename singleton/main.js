3c3db242f47f2c434526b0ecaea8d7332b222ed3
mireia <mireia@actualweb.es>
2018-05-15 08:36:26 +0200

    creating a demo singleton

---
+++ singleton/main.js
@@ -0,0 +1,11 @@
+var taskHandler = require('./taskHandler');
+var myrepo = require('./repo');
+// var myrepo = repo();
+
+myrepo.save('fromMain');
+myrepo.save('fromMain');
+myrepo.save('fromMain');
+taskHandler.save();
+taskHandler.save();
+taskHandler.save();
+taskHandler.save();
