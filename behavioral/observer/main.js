bf4161beb7115bab46296a8cbee96271f8901908
mireia <mireia@actualweb.es>
2018-05-16 09:29:42 +0200

    Adding the ObserverList object

--- behavioral/observer/main.js
+++ behavioral/observer/main.js
@@ -1,4 +1,19 @@
 var Task = require('./task');
+
+function ObserverList() {
+    this.observerList = [];
+}
+
+ObserverList.prototype.add = function (obj) {
+    return this.observerList.push(obj);
+};
+
+ObserverList.prototype.get = function (index) {
+    if(index > -1 && index < this.observerList.length ){
+        return this.observerList['index'];
+    }
+};
+
 var notificationService = function () {
     var message = 'Notifying ';
     this.update = function (task) {
